6713a072a3a75d3dce2af3a8e40c99c589e35620
Timothy Chai <timothy.chai@fdmgroup.com>
2024-07-11 15:00:43 +0800

    Resolve maven issues

---
+++ src/test/java/com/example/cafedb/bot/BotTests.java
@@ -0,0 +1,18 @@
+package com.example.cafedb.bot;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import static org.mockito.Mockito.*;
+
+public class BotTests {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+}
